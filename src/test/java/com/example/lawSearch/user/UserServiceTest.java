3a6821ae253c5a727598ff8f307d6b4dbb3e80c6
chanhee01 <joch4267@naver.com>
2024-02-11 18:22:36 +0900

    [Add] 비밀번호 수정 테스트 코드

--- src/test/java/com/example/lawSearch/user/UserServiceTest.java
+++ src/test/java/com/example/lawSearch/user/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.lawSearch.user;
 
 import com.example.lawSearch.domain.user.dto.request.UserRequestDto;
+import com.example.lawSearch.domain.user.dto.response.UserResponseDto;
 import com.example.lawSearch.domain.user.exception.EmailExistException;
 import com.example.lawSearch.domain.user.model.User;
 import com.example.lawSearch.domain.user.service.UserService;
@@ -9,15 +10,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Test
     @Transactional
@@ -43,5 +48,18 @@ public class UserServiceTest {
         UserRequestDto request2 = new UserRequestDto("email@naver.com", "pw", "lee", 30, false);
 
         assertThrows(EmailExistException.class, () -> userService.join(request2), "이메일 중복입니다.");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("비밀번호 변경")
+    void changePassword() {
+        UserRequestDto request = new UserRequestDto("email@naver.com", "password", "kim", 20, true);
+        userService.join(request);
+        User user = userService.findByEmail("email@naver.com");
+
+        userService.updatePassword(user.getId(), request.getPassword(), "newPassword");
+        boolean changed = bCryptPasswordEncoder.matches("newPassword", user.getPassword());
+        assertTrue(changed);
     }
 }

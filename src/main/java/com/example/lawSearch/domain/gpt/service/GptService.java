87a4025de6aad3851891752d2c0b631804b78d81
chanhee01 <joch4267@naver.com>
2024-02-13 19:49:41 +0900

    [Update] ai 요청 엔드포인트 변경

--- src/main/java/com/example/lawSearch/domain/gpt/service/GptService.java
+++ src/main/java/com/example/lawSearch/domain/gpt/service/GptService.java
@@ -2,9 +2,8 @@ package com.example.lawSearch.domain.gpt.service;
 
 import com.example.lawSearch.domain.gpt.dto.request.GptRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,14 +11,18 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class GptService {
 
+    @Value("${GPT_URL}")
+    private String GPT_URL;
+
     public String getBills(GptRequestDto request) {
         RestTemplate restTemplate = new RestTemplate();
-        String flaskUrl = "http://43.203.106.131:5000/api/send-data";
+        String flaskUrl = GPT_URL;
 
         HttpEntity<GptRequestDto> entity = new HttpEntity<>(request);
 
         // Flask 서버에 POST 요청 보내기
         ResponseEntity<String> response = restTemplate.exchange(flaskUrl, HttpMethod.POST, entity, String.class);
+
         return response.getBody();
     }
 }
